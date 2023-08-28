d65740ee22834e93d61747463e8f930e59a90716
wonmihyun <mihyun6656@gmail.com>
2023-08-28 14:26:34 +0900

    "로그인/로그아웃"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { Outlet } from 'react-router-dom';
 import './App.css';
 import Nav from './components/Nav';
+import GlobalStyle from './style/GlobalStyle';
 
 function App() {
   return (
     <>
+    <GlobalStyle/> 
+    {/* 전역변수 css 적용 컴포넌트 */}
     <Nav/> 
-    <Outlet/>
+    <Outlet/> 
+    {/* Outlet : children과 같은 효과 */}
     </>
   );
 }
