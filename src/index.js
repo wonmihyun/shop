ac0eae8b5a5515b25a4d03e05bd33928e454913e
wonmihyun <mihyun6656@gmail.com>
2023-08-28 12:09:46 +0900

    router-dom설정

--- src/index.js
+++ src/index.js
@@ -1,13 +1,59 @@
-import React from 'react';
+import React, { Children } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import NotFound from './pages/NotFound';
+import AllItems from './pages/NotFound';
+import NewItems from './pages/NotFound';
+import Cart from './pages/NotFound';
+
+
+/*
+  gh-pages로 연동하게 되면 주소 뒤에 /repository 이름이 붙게 된다.
+  ex) localhost:3000 => localhost:3000/repo명
+
+  그러나 리액트의 기본 주소는 /로 되어 있어서 경로가 달라지게 되는 걸 못받아온다.
+
+
+*/
+
+/* 설치
+  yarn add react-router-dom
+  yarn add react-icons
+  yarn add styled-components
+*/
+
+ 
+// 구버전 
+//  const router = createBrowserRouter([
+//     {
+//        path : '/', // home
+//        element : <App/> , // href
+//        errorElement : <NotFound/>,
+//        children : [
+//          {path : '/items', element:<AllItems/>},
+//          {path : '/items/new', element:<NewItems/>},
+//          {path : '/cart', element : <Cart/>}
+//   ]}
+//  ])
+
+// package.json안에 설정한 homepage의 주소를 자동으로 받아오는 명령어 
+const basename = process.env.PUBLIC_URL; 
+const routes = [
+  {path : '/', element : <App/>},
+  {path : '/items', element:<AllItems/>},
+  {path : '/items/new', element:<NewItems/>},
+  {path : '/cart', element : <Cart/>}
+]
+
+const router = createBrowserRouter(routes, {basename : basename});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
