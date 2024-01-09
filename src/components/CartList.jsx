20db1b0ca16daeb319e4896517c158234dd356c0
wonmihyun <mihyun6656@gmail.com>
2024-01-09 13:42:25 +0900

    "최종"

--- src/components/CartList.jsx
+++ src/components/CartList.jsx
@@ -11,7 +11,7 @@ export default function CartList({product, index}){
     const plusItem = () =>{
         addItemCart.mutate({...product, quantity : product.quantity + 1})
     }
-
+ 
     const minusItem = () =>{
         if(product.quantity < 2){
             alert('상품 갯수는 1보다 작을 수 없습니다.')
